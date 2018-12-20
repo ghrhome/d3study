cbdf7f50a236ff417e9d8779ee194ebf1a0af2e8
rockycheng82@gmail.com <rockycheng82@gmail.com>
2018-12-20 19:01:51 +0800

    update all

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import * as d3 from "d3"
 
+
 d3.selectAll("p")
     .data([4, 8, 15, 16, 23, 42])
     .style("font-size", function(d) { return d + "px"; })
@@ -8,50 +9,83 @@ d3.selectAll("p")
         return "I’m number " + d + "!";
         });
 
-
  //d3.select("p:nth-child(1)").remove();
 
+
 //柱状图
-var width = 300;  //画布的宽度
-var height = 300;   //画布的高度
+let width=500
+let height=500
 
-let rectHeight=30;
+let svg=d3.select("body")
+    .append("svg")
+    .attr("width", width)
+    .attr("height", height)
 
-var svg = d3.select("body")     //选择文档中的body元素
-    .append("svg")          //添加一个svg元素
-    .attr("width", width)       //设定宽度
-    .attr("height", height);    //设定高度
+let padding = {left:30, right:30, top:20, bottom:20};
 
-/*svg.selectAll("rect").data([20,30,40,50,60])
-    .enter()
-    .append("rect")
-    .attr("x",20)
-    .attr("y",function(d,i){
-        return i * rectHeight;
-    })
-    .attr("width",function(d){
-        return d;
-    })
-    .attr("height",rectHeight-2)
-    .attr("fill","steelblue");*/
+let dataset=[10, 20, 30, 40, 33, 24, 12, 5]
 
-var dataset = [1.2, 2.3, 0.9, 1.5, 3.3];
-var linear = d3.scale.linear()
-    .domain([0, d3.max(dataset)])
-    .range([0, 250]);
-svg.selectAll("rect")
+let xScale = d3.scaleOrdinal()
+    .domain([0, d3.range(dataset.length)])
+    .range([0, width - padding.left - padding.right]);
+
+let yScale= d3.scaleLinear()
+    .domain([0,d3.max(dataset)])
+    .rangeRound([0, height - padding.bottom -padding.top]);
+
+
+let xAxis = d3.axisBottom(xScale)
+let yAxis = d3.axisLeft(yScale);
+
+let rectPadding = 4;
+let rects= svg.selectAll(".myRect")
     .data(dataset)
     .enter()
     .append("rect")
-    .attr("x",20)
-    .attr("y",function(d,i){
-        return i * rectHeight;
+    .attr('class' , 'myRect')
+    .attr("transform", "translate(" + padding.left + "," + padding.top + ")")
+    .attr('x', function(d,i){
+        return xScale(i) +rectPadding/2
     })
-    .attr("width",function(d){
-        return linear(d);   //在这里用比例尺
+    .attr('y',function(d){
+        return yScale(d);
     })
-    .attr("height",rectHeight-2)
-    .attr("fill","steelblue");
+    .attr("width", xScale.rangeBand() - rectPadding )
+    .attr("height", function(d){
+        return height - padding.top - padding.bottom - yScale(d);
+    });
 
 
+let texts = svg.selectAll(".MyText")
+    .data(dataset)
+    .enter()
+    .append("text")
+    .attr("class","MyText")
+    .attr("transform","translate(" + padding.left + "," + padding.top + ")")
+    .attr("x", function(d,i){
+        return xScale(i) + rectPadding/2;
+    } )
+    .attr("y",function(d){
+        return yScale(d);
+    })
+    .attr("dx",function(){
+        return (xScale.rangeBand() - rectPadding)/2;
+    })
+    .attr("dy",function(d){
+        return 20;
+    })
+    .text(function(d){
+        return d;
+    });
 
+//添加x轴
+svg.append("g")
+    .attr("class","axis")
+    .attr("transform","translate(" + padding.left + "," + (height - padding.bottom) + ")")
+    .call(xAxis);
+
+//添加y轴
+svg.append("g")
+    .attr("class","axis")
+    .attr("transform","translate(" + padding.left + "," + padding.top + ")")
+    .call(yAxis);
---
+++ src/index_test.js
@@ -0,0 +1,72 @@
+import * as d3 from "d3"
+
+
+d3.selectAll("p")
+    .data([4, 8, 15, 16, 23, 42])
+    .style("font-size", function(d) { return d + "px"; })
+    .text(function(d,i) {
+        console.log(i);
+        return "I’m number " + d + "!";
+        });
+
+
+ //d3.select("p:nth-child(1)").remove();
+
+//柱状图
+var width = 300;  //画布的宽度
+var height = 300;   //画布的高度
+
+let rectHeight=30;
+
+var svg = d3.select("body")     //选择文档中的body元素
+    .append("svg")          //添加一个svg元素
+    .attr("width", width)       //设定宽度
+    .attr("height", height);    //设定高度
+
+/*svg.selectAll("rect").data([20,30,40,50,60])
+    .enter()
+    .append("rect")
+    .attr("x",20)
+    .attr("y",function(d,i){
+        return i * rectHeight;
+    })
+    .attr("width",function(d){
+        return d;
+    })
+    .attr("height",rectHeight-2)
+    .attr("fill","steelblue");*/
+
+var dataset = [1.2, 2.3, 0.9, 1.5, 3.3];
+console.log(d3.max(dataset));
+var linear = d3.scaleLinear()
+    .domain([0, d3.max(dataset)])
+    .rangeRound([0, 250]);
+
+
+svg.selectAll("rect")
+    .data(dataset)
+    .enter()
+    .append("rect")
+    .attr("x",20)
+    .attr("y",function(d,i){
+        return i * rectHeight;
+    })
+    .attr("width",function(d){
+        console.log(d)
+        console.log(linear(d))
+        return linear(d);   //在这里用比例尺
+    })
+    .attr("height",rectHeight-2)
+    .attr("fill","steelblue");
+
+var axis = d3.axisBottom(linear)//指定比例尺
+    .ticks(7);          //指定刻度的数量
+
+svg.append("g")
+    .attr("class","axis")
+    .attr("transform","translate(20,150)")
+    .call(axis);
+
+
+
+
